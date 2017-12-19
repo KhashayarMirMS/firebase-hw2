b55714a3ea19e83cf99f5eec50bb03b054de3b1e
Khashayar MirMS <mms.khashayar@gmail.com>
2017-12-19 12:08:34 +0330

    final integration to react format

---
+++ firebase-copy/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/main.css';
+import './css/materials.css'
+import './css/icons.css'
+
+ReactDOM.render(
+    <div>hello</div>,
+    document.body
+);
